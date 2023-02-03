ab95da46ab6436115d19570a9435eef6cf50ef3c
Fuaark <largeladphil@outlook.com>
2023-02-03 23:26:07 +1100

    typo

--- _data/main.js
+++ _data/main.js
@@ -2,7 +2,7 @@ module.exports = {
     en: {
         welcome: "Welcome",
         siteDescription: `<p>On this website, I document my progress towards becoming a Data Scientist and showcase my learning and projects along the way.</p>
-                          <p>I mainly do this for myself but dearly hope that someone might find some insights</p>`,
+                          <p>I mainly do this for myself, but dearly hope that someone might find some insights</p>`,
         latest: 'latest',
         post: 'post',
         posts: 'posts',
