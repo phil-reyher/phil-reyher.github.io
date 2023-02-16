246dd937c7008e56edf1e3cff9890f2cf145b010
Fuaaark <largeladphil@outlook.com>
2023-02-16 13:59:33 +1100

    change post to project

--- _data/main.js
+++ _data/main.js
@@ -4,7 +4,7 @@ module.exports = {
         siteDescription: `<p>On this website, I document my progress towards becoming a Data Scientist and showcase my learning and projects along the way.</p>
                           <p>I mainly do this for myself, but dearly hope that someone might find some insights</p>`,
         latest: 'latest',
-        post: 'post',
-        posts: 'posts',
+        post: 'project',
+        posts: 'projects',
     }
 }
