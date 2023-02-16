a62666427a204897eb23e2ce8ce7add026535b53
Fuaaark <largeladphil@outlook.com>
2023-02-16 15:28:13 +1100

    Add social media buttons

---
+++ _data/buttons.js
@@ -0,0 +1,5 @@
+module.exports = [
+    { title: 'Github', icon: 'brand-github', link: 'https://github.com' },
+    { title: 'Email', icon: 'brand-telegram', link: 'mailto:you@example.com' }
+  ];
+  
