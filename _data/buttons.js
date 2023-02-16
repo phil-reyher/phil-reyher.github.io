3479057032cd048d8817763e2193aff58dea6a26
Fuaaark <largeladphil@outlook.com>
2023-02-16 16:51:05 +1100

    Media buttons, tooltip to be added

--- _data/buttons.js
+++ _data/buttons.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { title: 'Github', icon: 'brand-github', link: 'https://github.com' },
-    { title: 'Email', icon: 'brand-telegram', link: 'mailto:you@example.com' }
+    { title: 'github', icon: 'brand-github', link: 'https://github.com/phil-reyher',tooltip:'Visit my GitHub' },
+    { title: 'email', icon: 'brand-telegram', link: 'mailto:philipp@reyher.io',tooltip: 'Shoot me an email' }
   ];
   
