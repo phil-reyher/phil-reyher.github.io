d5ec8bd40abe02b76170caa23296521ace1f5e98
Fuaaark <largeladphil@outlook.com>
2023-03-10 14:28:45 +1100

    Footer social media buttons

--- _data/buttons.js
+++ _data/buttons.js
@@ -1,5 +1,5 @@
 module.exports = [
-    { title: 'github', icon: 'brand-github', link: 'https://github.com/phil-reyher',tooltip:'My GitHub' },
-    { title: 'email', icon: 'email', link: 'mailto:philipp@reyher.io',tooltip: 'Email Me' }
+    { title: 'github', icon: 'brand-github', link: 'https://github.com/phil-reyher',tooltip:'GitHub' },
+    { title: 'email', icon: 'email', link: 'mailto:philipp@reyher.io',tooltip: 'Email' }
   ];
   
