00e89bd8615de50252c1caa74c987da3da5f5184
Fuaaark <largeladphil@outlook.com>
2023-03-10 18:39:30 +1100

    Variable scrollpadding

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,16 @@
 console.log("%cHELLO, WORLD!!!", "color: green");
+
+
+let headerHeight;
+
+window.onload = function() {
+    // Get the header element
+    const header = document.querySelector(".c-header");
+
+    // Get the height of the header
+    const headerHeight = header.offsetHeight;
+
+    // Set the scroll padding top on the body element
+    document.documentElement.style.scrollPaddingTop = headerHeight + 16 +"px";
+    console.log(headerHeight);
+  }
