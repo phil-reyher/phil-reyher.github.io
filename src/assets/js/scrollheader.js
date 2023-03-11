8d5402cd5707c3156aee791c06bbbc13adfbf42a
Fuaaark <largeladphil@outlook.com>
2023-03-11 16:48:17 +1100

    hide header on scroll script

--- src/assets/js/scrollheader.js
+++ src/assets/js/scrollheader.js
@@ -2,20 +2,19 @@ var prevScrollpos = window.pageYOffset;
 
 /* Get the header element and it's position */
 var headerDiv = document.querySelector("header");
-var headerBottom = headerDiv.offsetTop + headerDiv.offsetHeight;
+var headerHeight = headerDiv.offsetHeight+4;
+var headerBottom = headerDiv.offsetTop + headerHeight;
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
   /* if scrolling down, let it scroll out of view as normal */
   if (prevScrollpos <= currentScrollPos ){
-      headerDiv.classList.remove("fixedToTop");
-      headerDiv.style.top ="-7.2rem";
+    headerDiv.style.transform = "translateY(-" + headerHeight + "px)";
   }
   /* otherwise if we're scrolling up, fix the nav to the top */
   else{  
-      headerDiv.classList.add("fixedToTop");
-      headerDiv.style.top = "0";
+    headerDiv.style.transform = "none";
   }
 
   prevScrollpos = currentScrollPos;
