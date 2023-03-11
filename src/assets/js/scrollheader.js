6392c79dcd9bb6856e397348e10f39cdb3830208
Fuaaark <largeladphil@outlook.com>
2023-03-11 17:09:48 +1100

    only hide after scrolling past headerheight

--- src/assets/js/scrollheader.js
+++ src/assets/js/scrollheader.js
@@ -1,15 +1,17 @@
 var prevScrollpos = window.pageYOffset;
 
 /* Get the header element and it's position */
+var headerBar = document.querySelector('.c-header-bar');
+var headerBarHeight = headerBar.offsetHeight;
 var headerDiv = document.querySelector("header");
-var headerHeight = headerDiv.offsetHeight+4;
+var headerHeight = headerDiv.offsetHeight+headerBarHeight;
 var headerBottom = headerDiv.offsetTop + headerHeight;
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
 
   /* if scrolling down, let it scroll out of view as normal */
-  if (prevScrollpos <= currentScrollPos ){
+  if (prevScrollpos <= currentScrollPos && currentScrollPos > headerHeight ){
     headerDiv.style.transform = "translateY(-" + headerHeight + "px)";
   }
   /* otherwise if we're scrolling up, fix the nav to the top */
