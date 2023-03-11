b0fddb202d58ad91dfad7940d915b45b3b77621f
phil-reyher <phil-reyher@users.noreply.github.com>
2023-03-11 10:02:30 +0000

    deploy: 455f706b699beb218a0d2518025034a80901a343

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,1 +1,1 @@
-!function(){"use strict";var e=window.pageYOffset,o=document.querySelector(".c-header-bar").offsetHeight,t=document.querySelector("header"),n=t.offsetHeight+o;t.offsetTop,window.onscroll=function(){var o=window.pageYOffset;t.style.transform=e<=o&&o>n?"translateY(-"+n+"px)":"none",e=o},console.log("%cHELLO, WORLD!!!","color: green")}();
+!function(){"use strict";console.log("%cHELLO, WORLD!!!","color: green")}();
