06b8af89dec055249f4867f845879caab1fa767b
Fuaaark <largeladphil@outlook.com>
2023-03-14 12:06:51 +1100

    add mastodon

--- _data/buttons.js
+++ _data/buttons.js
@@ -1,5 +1,6 @@
 module.exports = [
     { title: 'github', icon: 'brand-github', link: 'https://github.com/phil-reyher',tooltip:'GitHub' },
-    { title: 'email', icon: 'email', link: 'mailto:philipp@reyher.io',tooltip: 'Email' }
+    { title: 'email', icon: 'email', link: 'mailto:philipp@reyher.io',tooltip: 'Email' },
+    { title: 'mastodon', icon: 'brand-mastodon', link: 'https://mastodon.social/@reyher',tooltip: 'Mastodon' }
   ];
   
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,5 @@
 console.log("%cHELLO, WORLD!!!", "color: green");
+
+var mastodonLink = document.querySelector('a[title="mastodon"]');
+// Add the "rel" attribute with a value of "me" to the Mastodon link element
+mastodonLink.setAttribute('rel', 'me');
