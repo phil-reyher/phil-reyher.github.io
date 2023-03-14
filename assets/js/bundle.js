a40e100d5756947cb534574d6ebe59faa96375ae
phil-reyher <phil-reyher@users.noreply.github.com>
2023-03-14 01:07:31 +0000

    deploy: 4bd932d7a777e535f511d28b3d4c6ec30c7ff09f

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,1 +1,1 @@
-!function(){"use strict";console.log("%cHELLO, WORLD!!!","color: green")}();
+!function(){"use strict";console.log("%cHELLO, WORLD!!!","color: green"),document.querySelector('a[title="mastodon"]').setAttribute("rel","me")}();
