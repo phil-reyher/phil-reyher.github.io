8ac314146cec7601b3fdec959d2382a6a4f19694
Philipp Reyher <largeladphil@outlook.com>
2023-04-13 11:52:56 +0200

    New Buttons

--- _data/buttons.js
+++ _data/buttons.js
@@ -1,6 +1,8 @@
 module.exports = [
     { title: 'github', icon: 'brand-github', link: 'https://github.com/phil-reyher',tooltip:'GitHub' },
+    { title: 'gitlab', icon: 'brand-gitlab', link: 'https://gitlab.com/phil-reyher',tooltip: 'GitLab' },
     { title: 'email', icon: 'email', link: 'mailto:philipp@reyher.io',tooltip: 'Email' },
+    { title: 'linkedin', icon: 'brand-linkedin', link: 'TBA',tooltip: 'LinkedIn' },
     { title: 'mastodon', icon: 'brand-mastodon', link: 'https://mastodon.social/@reyher',tooltip: 'Mastodon' }
   ];
   
